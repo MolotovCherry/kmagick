9e40bf09919cfd6b139ae59608a8381bc9efb9e0
Cherryleafroad <13651622+cherryleafroad@users.noreply.github.com>
2021-09-24 17:54:46 -0700

    Fix the generator

--- gen-jni.ps1
+++ gen-jni.ps1
@@ -6,7 +6,7 @@ $ktFiles = Join-String -InputObject $files -Separator " "
 kotlinc $ktFiles.Split(" ")
 
 foreach ($f in $files) {
-    javah -jni -force -cp . com.cherryleafroad.jmagick.$($f.BaseName)
+    javah -jni -force -cp . com.cherryleafroad.kmagick.$($f.BaseName)
 }
 
 Remove-Item -Recurse META-INF
