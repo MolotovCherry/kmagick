cf08e9a20f098136ac958da96241526514a10cdb
Cherryleafroad <13651622+cherryleafroad@users.noreply.github.com>
2021-10-12 16:33:55 -0700

    Semi-example now

--- example/src/com/example/cli/Main.kt
+++ example/src/com/example/cli/Main.kt
@@ -1,19 +1,34 @@
 package com.example.cli
 
-import com.cherryleafroad.kmagick.Magick
-import com.cherryleafroad.kmagick.MagickWand
+import com.cherryleafroad.kmagick.*
 
 fun main() {
+    // close resources when finished
     Magick.initialize().use {
-        println("new wand")
-        var wand = MagickWand()
-        //wand.new()
-        println("destroyed wand")
-        wand.destroy()
-        println("made new wand -> handle")
-        wand = MagickWand()
-        println("cloning wand")
-        val newWand = wand.clone()
-        println("cloned handle")
+        val wand = MagickWand()
+
+        val pw = PixelWand()
+        pw.color = "#ff0000"
+        wand.newImage(500, 500, pw)
+
+        wand.getImagePixelColor(100, 100)?.let {
+            //srgb(255,0,0)
+            println(it.color)
+        }
+
+        val dwand = DrawingWand()
+        dwand.fontSize = 40.0
+        dwand.font = "Times New Roman"
+
+        val colorWand = PixelWand()
+        colorWand.color = "black"
+        dwand.fillColor = colorWand
+        wand.annotateImage(dwand, 0.0,50.0, 0.0, "Some text")
+
+        // get bytes of image, then read it back in
+        val blob = wand.writeImageBlob("png")
+        wand.readImageBlob(blob)
+
+        wand.writeImage("D:/out.png")
     }
 }
