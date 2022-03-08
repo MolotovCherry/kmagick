a55847da4a24a1fd8720a10c5d22d969c66cab7c
Cherryleafroad <13651622+cherryleafroad@users.noreply.github.com>
2022-03-07 19:36:33 -0800

    Fix build.rs and android build failures

--- rust/kmagick/build.rs
+++ rust/kmagick/build.rs
@@ -1,11 +1,12 @@
-#[cfg(windows)]
+use std::env;
 use winres;
 
-#[cfg(windows)]
 fn main() {
+    let is_windows = env::var("CARGO_CFG_WINDOWS").is_ok();
+    if !is_windows {
+        return
+    }
+
     let res = winres::WindowsResource::new();
     res.compile().unwrap();
 }
-
-#[cfg(not(windows))]
-fn main() {}
